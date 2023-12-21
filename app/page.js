efef24a8eb248b0b95d785ab7fcf3379327d854d
AlfredoArizmende <k.arizmende@gmail.com>
2023-12-20 20:06:23 -0600

    Hero section

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import Navbar from "./ui/Navbar";
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
 
 export default function Home() {
   return (
     <div className="bg-primary-black overflow-hidden">
       <Navbar />
+      <Hero />
     </div>
   );
 }
