8e5a0f546c82d6703a76c2c00c878b6a0392a4c4
AlfredoArizmende <k.arizmende@gmail.com>
2023-12-21 14:37:07 -0600

    About and Explore section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
-import Image from "next/image";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Explore from "./sections/Explore";
 
 export default function Home() {
   return (
     <div className="bg-primary-black overflow-hidden">
       <Navbar />
       <Hero />
+      <div className="relative">
+        <About />
+        <div className="gradient-03 z-0" />
+        <Explore />
+      </div>
     </div>
   );
 }
